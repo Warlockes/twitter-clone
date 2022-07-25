899724b7ecce99a56fcede544a80a317b7a773f5
Alex Warlockes <kirushckin.alexei@gmail.com>
2022-07-25 13:37:50 +0300

    убрал прокси, добавил baseUrl

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -2,7 +2,9 @@ import axios, { AxiosRequestConfig } from "axios";
 
 axios.interceptors.request.use(
   (config: AxiosRequestConfig): AxiosRequestConfig => {
+    config.baseURL = "https://alex-twitter-clone-backend.herokuapp.com";
     const token = window.localStorage.getItem("twitter-clone_token");
+
     if (!config.headers) {
       config.headers = {};
     }
